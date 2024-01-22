bf9b60a1a5ab993d9408e7fbeb8c4a09c0b15d90
Nil <95134227+NirejBatrai@users.noreply.github.com>
2024-01-22 13:58:05 +0700

    add images and its desciption

--- renting-app/src/pages/Pricing.jsx
+++ renting-app/src/pages/Pricing.jsx
@@ -1,7 +1,9 @@
 import Card from "../components/Card";
 import Image1 from "../assets/Tmax.png";
-import Image2 from "../assets/XADV.png";
-s;
+import Image2 from "../assets/Xadv-750.png";
+import Image3 from "../assets/PCX.png";
+import Image4 from "../assets/Xmaxx.png";
+import Image5 from "../assets/Tmaxx.png";
 import "./Pricing.css";
 
 export default function Pricing() {
@@ -12,7 +14,7 @@ export default function Pricing() {
       <div className="card-container">
         <Card
           imageSrc={Image1}
-          bikeName={"T-max"}
+          bikeName={"T-max 550cc"}
           description={
             <p>
               Unleash the urban warrior within with the Tmax bike. Featuring a
@@ -29,7 +31,7 @@ export default function Pricing() {
 
         <Card
           imageSrc={Image2}
-          bikeName={"XADV"}
+          bikeName={"XADV 750cc"}
           description={
             <p>
               Embark on a journey of boundless exploration with the Xadv bike,
@@ -46,8 +48,43 @@ export default function Pricing() {
         />
 
         <Card
-          imageSrc={Image2}
-          bikeName={"X-Max"}
+          imageSrc={Image3}
+          bikeName={"PCX 160cc"}
+          description={
+            <p>
+              The PCX motorbike is a sleek and stylish two-wheeler renowned for
+              its performance and efficiency. Powered by a 160cc engine, it
+              seamlessly blends power with fuel efficiency, making it an ideal
+              choice for urban commuting. Its modern design, comfortable
+              seating, and advanced features make the PCX a standout option for
+              riders seeking a dynamic and practical riding experience. Whether
+              navigating through city traffic or cruising on the open road, the
+              PCX delivers a thrilling ride while maintaining optimal fuel
+              economy.
+            </p>
+          }
+        />
+
+        <Card
+          imageSrc={Image4}
+          bikeName={"X-Max 300cc"}
+          description={
+            <p>
+              Unleash the urban warrior within with the XMAX scooter. Boasting a
+              responsive 300cc engine, this sleek marvel is more than just a
+              means of transport; it's a bold statement. The XMAX seamlessly
+              merges sporty design with cutting-edge features, offering an
+              unparalleled fusion of style and performance. Whether navigating
+              busy city streets or embarking on a weekend adventure, the XMAX
+              delivers an unbeatable blend of comfort and exhilaration. Indulge
+              in the flawless synthesis of efficiency and elegance with the XMAX
+              scooter.
+            </p>
+          }
+        />
+        <Card
+          imageSrc={Image5}
+          bikeName={"T-max 550cc"}
           description={
             <p>
               Unleash the urban warrior within with the Tmax bike. Featuring a
@@ -64,7 +101,7 @@ export default function Pricing() {
 
         <Card
           imageSrc={Image2}
-          bikeName={"XADV"}
+          bikeName={"XADV 750cc"}
           description={
             <p>
               Embark on a journey of boundless exploration with the Xadv bike,
@@ -76,6 +113,112 @@ export default function Pricing() {
               terrain it encounters. Elevate your riding experience with the
               Xadv and embrace the excitement of uncharted paths with unwavering
               confidence.
+            </p>
+          }
+        />
+
+        <Card
+          imageSrc={Image3}
+          bikeName={"PCX 160cc"}
+          description={
+            <p>
+              The PCX motorbike is a sleek and stylish two-wheeler renowned for
+              its performance and efficiency. Powered by a 160cc engine, it
+              seamlessly blends power with fuel efficiency, making it an ideal
+              choice for urban commuting. Its modern design, comfortable
+              seating, and advanced features make the PCX a standout option for
+              riders seeking a dynamic and practical riding experience. Whether
+              navigating through city traffic or cruising on the open road, the
+              PCX delivers a thrilling ride while maintaining optimal fuel
+              economy.
+            </p>
+          }
+        />
+
+        <Card
+          imageSrc={Image4}
+          bikeName={"X-Max 300cc"}
+          description={
+            <p>
+              Unleash the urban warrior within with the XMAX scooter. Boasting a
+              responsive 300cc engine, this sleek marvel is more than just a
+              means of transport; it's a bold statement. The XMAX seamlessly
+              merges sporty design with cutting-edge features, offering an
+              unparalleled fusion of style and performance. Whether navigating
+              busy city streets or embarking on a weekend adventure, the XMAX
+              delivers an unbeatable blend of comfort and exhilaration. Indulge
+              in the flawless synthesis of efficiency and elegance with the XMAX
+              scooter.
+            </p>
+          }
+        />
+        <Card
+          imageSrc={Image1}
+          bikeName={"T-max 550cc"}
+          description={
+            <p>
+              Unleash the urban warrior within with the Tmax bike. Featuring a
+              responsive 560cc engine, this sleek marvel is not just a mode of
+              transportation; it is a statement. With its sporty design and
+              cutting-edge features, the Tmax seamlessly blends style with
+              performance. Whether you are weaving through city streets or
+              embarking on a weekend joyride, the Tmax offers an unmatched
+              combination of comfort and exhilaration. Experience the perfect
+              synthesis of efficiency and elegance with the Tmax bike.
+            </p>
+          }
+        />
+
+        <Card
+          imageSrc={Image2}
+          bikeName={"XADV 750cc"}
+          description={
+            <p>
+              Embark on a journey of boundless exploration with the Xadv bike,
+              your versatile adventure companion. Powering through with a robust
+              750cc engine, this off-road marvel is not just a bike; it is a
+              testament to the spirit of adventure. Built for those who crave
+              the thrill of the unknown, the Xadv boasts a rugged design,
+              advanced suspension, and powerful performance to conquer every
+              terrain it encounters. Elevate your riding experience with the
+              Xadv and embrace the excitement of uncharted paths with unwavering
+              confidence.
+            </p>
+          }
+        />
+
+        <Card
+          imageSrc={Image3}
+          bikeName={"PCX 160cc"}
+          description={
+            <p>
+              The PCX motorbike is a sleek and stylish two-wheeler renowned for
+              its performance and efficiency. Powered by a 160cc engine, it
+              seamlessly blends power with fuel efficiency, making it an ideal
+              choice for urban commuting. Its modern design, comfortable
+              seating, and advanced features make the PCX a standout option for
+              riders seeking a dynamic and practical riding experience. Whether
+              navigating through city traffic or cruising on the open road, the
+              PCX delivers a thrilling ride while maintaining optimal fuel
+              economy.
+            </p>
+          }
+        />
+
+        <Card
+          imageSrc={Image4}
+          bikeName={"X-Max 300cc"}
+          description={
+            <p>
+              Unleash the urban warrior within with the XMAX scooter. Boasting a
+              responsive 300cc engine, this sleek marvel is more than just a
+              means of transport; it's a bold statement. The XMAX seamlessly
+              merges sporty design with cutting-edge features, offering an
+              unparalleled fusion of style and performance. Whether navigating
+              busy city streets or embarking on a weekend adventure, the XMAX
+              delivers an unbeatable blend of comfort and exhilaration. Indulge
+              in the flawless synthesis of efficiency and elegance with the XMAX
+              scooter.
             </p>
           }
         />
