7b4fea6699cbddb2a97718f47a13574d4a3466a5
Nil <95134227+NirejBatrai@users.noreply.github.com>
2024-01-22 13:56:30 +0700

    move button style into css file

--- renting-app/src/components/Card.jsx
+++ renting-app/src/components/Card.jsx
@@ -7,16 +7,6 @@ export default function Card({ imageSrc, bikeName, description }) {
     Width: "100px",
   };
 
-  const buttonStyle = {
-    backgroundColor: "#3498db", // Use your desired button color
-    color: "#fff", // Use a contrasting text color
-    padding: "10px 15px",
-    border: "none",
-    borderRadius: "5px",
-    cursor: "pointer",
-    transition: "background-color 0.3s ease",
-  };
-
   return (
     <div className="card-holder">
       <img src={imageSrc} alt={description} />
@@ -25,7 +15,7 @@ export default function Card({ imageSrc, bikeName, description }) {
 
       <p style={{ fontSize: "14px" }}>{description}</p>
 
-      <button style={buttonStyle}>Learn more</button>
+      <button className="button-style">Learn more</button>
     </div>
   );
 }
