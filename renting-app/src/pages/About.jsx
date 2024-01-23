89cf20f32e84a0150b195fc4debbbb83f21fd70d
Nil <95134227+NirejBatrai@users.noreply.github.com>
2024-01-23 14:33:07 +0700

    Add h1 in about page

--- renting-app/src/pages/About.jsx
+++ renting-app/src/pages/About.jsx
@@ -1,7 +1,11 @@
+import "./About.css";
+
 export default function About() {
   return (
     <>
-      <h1>About</h1>
+      <div className="about-container">
+        <h1>about</h1>
+      </div>
     </>
   );
 }
