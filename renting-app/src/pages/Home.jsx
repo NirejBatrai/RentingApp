740486819a7bb321da960dfa40b654241a4b8f24
Nil <95134227+NirejBatrai@users.noreply.github.com>
2024-01-23 14:34:18 +0700

    add img text and button in home page

--- renting-app/src/pages/Home.jsx
+++ renting-app/src/pages/Home.jsx
@@ -1,3 +1,27 @@
+import Image2 from "../assets/Xadv-750.png";
+import "./Home.css";
+
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <div className="home-container">
+      <div className="landing-section">
+        <img src={Image2} alt="Xadv motorbike image" />
+
+        <h1>Some good shit for you</h1>
+
+        <div className="buttons-container">
+          <button>Learn more</button>
+          <button>Pick a bike</button>
+          <button>Fuckz</button>
+        </div>
+      </div>
+    </div>
+  );
+  // Top Image
+
+  // Slogan
+
+  //Two buttons for the
+
+  // Some basic info
 }
