08d1f6f07c15122fd30ff702373c32cbc9fefe86
Nil <95134227+NirejBatrai@users.noreply.github.com>
2024-01-23 14:32:07 +0700

    move navbar to component files

---
+++ renting-app/src/components/Navbar.jsx
@@ -0,0 +1,36 @@
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
+import "./Navbar.css";
+
+export default function Navbar() {
+  return (
+    <nav className="nav">
+      <Link to="/" className="site-title">
+        Site Name
+      </Link>
+      <ul>
+        <li>
+          <CustomLink to="/">Home</CustomLink>
+        </li>
+        <li>
+          <CustomLink to="/pricing">Pricing</CustomLink>
+        </li>
+        <li>
+          <CustomLink to="/about">About</CustomLink>
+        </li>
+        <li>
+          <CustomLink to="/login">Login</CustomLink>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+function CustomLink({ to, children, ...props }) {
+  const resolevedPath = useResolvedPath(to);
+  const isActive = useMatch({ path: resolevedPath.pathname, end: true });
+  return (
+    <Link to={to} className={isActive ? "active" : ""} {...props}>
+      {children}
+    </Link>
+  );
+}
