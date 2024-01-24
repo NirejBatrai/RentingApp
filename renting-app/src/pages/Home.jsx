4a7079022ede1d161c0958f5bc23a4e1eaddaf2d
Nil <95134227+NirejBatrai@users.noreply.github.com>
2024-01-24 14:25:01 +0700

    add some react-spring animation on text image and button

--- renting-app/src/pages/Home.jsx
+++ renting-app/src/pages/Home.jsx
@@ -1,27 +1,46 @@
+import React from "react";
 import Image2 from "../assets/Xadv-750.png";
 import "./Home.css";
+import { useSpring, animated } from "react-spring";
 
 export default function Home() {
+  const imageSpring = useSpring({
+    opacity: 1,
+    from: { opacity: 0 },
+    transform: "scale(1)",
+    delay: 600,
+    // Add more animation properties as needed
+  });
+
+  const textSpring = useSpring({
+    opacity: 2,
+    from: { opacity: 0 },
+    delay: 500,
+  });
+
+  const buttonSpring = useSpring({
+    from: { opacity: 0, transform: "translateY(30px)" },
+    to: { opacity: 1, transform: "translateY(0)" },
+    config: { duration: 500 },
+  });
+
   return (
     <div className="home-container">
       <div className="landing-section">
-        <img src={Image2} alt="Xadv motorbike image" />
+        <animated.img
+          src={Image2}
+          alt="Xadv motorbike image"
+          style={imageSpring}
+        />
 
-        <h1>Some good shit for you</h1>
+        <animated.h1 style={textSpring}>Some good shit for you</animated.h1>
 
         <div className="buttons-container">
-          <button>Learn more</button>
-          <button>Pick a bike</button>
-          <button>Fuckz</button>
+          <animated.button style={buttonSpring}>Learn more</animated.button>
+          <animated.button style={buttonSpring}>Pick a bike</animated.button>
+          <animated.button style={buttonSpring}>F***z</animated.button>
         </div>
       </div>
     </div>
   );
-  // Top Image
-
-  // Slogan
-
-  //Two buttons for the
-
-  // Some basic info
 }
