991310ff0d7a1cec93621b997fed1065b7035290
Nil <95134227+NirejBatrai@users.noreply.github.com>
2024-01-24 15:12:49 +0700

    add button style

--- renting-app/src/pages/Home.jsx
+++ renting-app/src/pages/Home.jsx
@@ -6,7 +6,8 @@ import { useSpring, animated } from "react-spring";
 export default function Home() {
   const imageSpring = useSpring({
     opacity: 1,
-    from: { opacity: 0 },
+    from: { opacity: 0, transform: "translateX(-160px)" },
+    to: { opacity: 1, transform: "translateX(0)" },
     transform: "scale(1)",
     delay: 600,
     // Add more animation properties as needed
@@ -14,13 +15,14 @@ export default function Home() {
 
   const textSpring = useSpring({
     opacity: 2,
-    from: { opacity: 0 },
+    from: { opacity: 0, transform: "translateX(160px)" },
+    to: { opacity: 1, transform: "translateX(0)" },
     delay: 500,
   });
 
   const buttonSpring = useSpring({
-    from: { opacity: 0, transform: "translateY(30px)" },
-    to: { opacity: 1, transform: "translateY(0)" },
+    from: { opacity: 0, transform: "translateX(-160px)" },
+    to: { opacity: 1, transform: "translateX(0)" },
     config: { duration: 500 },
   });
 
@@ -36,9 +38,15 @@ export default function Home() {
         <animated.h1 style={textSpring}>Some good shit for you</animated.h1>
 
         <div className="buttons-container">
-          <animated.button style={buttonSpring}>Learn more</animated.button>
-          <animated.button style={buttonSpring}>Pick a bike</animated.button>
-          <animated.button style={buttonSpring}>F***z</animated.button>
+          <animated.button className="button-55" style={buttonSpring}>
+            Learn more
+          </animated.button>
+          <animated.button className="button-55" style={buttonSpring}>
+            Pick a bike
+          </animated.button>
+          <animated.button className="button-55" style={buttonSpring}>
+            F***z
+          </animated.button>
         </div>
       </div>
     </div>
