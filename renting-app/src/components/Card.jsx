fda11629b4867c893f6a8e507fb85eb82b8faafd
Nil <95134227+NirejBatrai@users.noreply.github.com>
2024-01-26 17:18:20 +0700

    fixed the error of passing data ...imagesrc and description  to BikeDescription page

--- renting-app/src/components/Card.jsx
+++ renting-app/src/components/Card.jsx
@@ -1,17 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Card.css";
 
 export default function Card({ imageSrc, bikeName, description }) {
   return (
     <div className="card-holder">
-      <img src={imageSrc} alt={description} />
-
+      <img src={imageSrc} alt={bikeName} />
       <h1>{bikeName}</h1>
-
-      <p style={{ fontSize: "14px" }}>{description}</p>
-
+      <p>{description}</p>
       <div className="card-buttons-container">
-        <button className="button-style">Learn more</button>
+        <Link
+          to={`/bike-details?imageSrc=${encodeURIComponent(
+            imageSrc
+          )}&bikeName=${encodeURIComponent(
+            bikeName
+          )}&description=${encodeURIComponent(description)}`}
+          className="button-style"
+        >
+          Details
+        </Link>
         <button className="button-style">Select</button>
       </div>
     </div>
