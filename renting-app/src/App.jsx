2da7d4e1750b08d9d79895ca123bc5723eff4246
Nil <95134227+NirejBatrai@users.noreply.github.com>
2024-01-26 17:16:53 +0700

    added BikeDetails Page and add it path in App.jsx page

--- renting-app/src/App.jsx
+++ renting-app/src/App.jsx
@@ -5,6 +5,7 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Login from "./pages/Login";
 import { Route, Routes } from "react-router-dom";
+import BikeDetails from "./pages/BIkeDetails";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="/pricing" element={<Pricing />} />
           <Route path="/about" element={<About />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/bike-details" element={<BikeDetails />} />
         </Routes>
       </div>
     </>
