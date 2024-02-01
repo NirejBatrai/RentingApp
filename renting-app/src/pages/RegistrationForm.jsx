667dcb31ca42529be61962457b1c8347bb0db183
Nil <95134227+NirejBatrai@users.noreply.github.com>
2024-02-01 23:27:58 +0700

    added handleRegistration from login page and connect with server email and pass

--- renting-app/src/pages/RegistrationForm.jsx
+++ renting-app/src/pages/RegistrationForm.jsx
@@ -5,20 +5,47 @@ const RegistrationForm = ({ onClose }) => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [confirmPassword, setconfirmPassword] = useState("");
 
-  const handleRegister = () => {
-    // Handle registration logic here
-    // You may want to send the registration data to the server
-    // Close the registration form after registration is successful
+  const handleRegister = async () => {
+    await handleRegistration();
     onClose();
   };
 
+  const handleRegistration = async () => {
+    console.log(email);
+    console.log(confirmPassword);
+
+    // Validate email and password
+    if (!email || !confirmPassword) {
+      console.error("Email and password are required");
+      return;
+    }
+
+    try {
+      const response = await fetch("http://localhost:5001/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: email,
+          password: confirmPassword,
+        }),
+      });
+      const data = await response.json();
+      console.log(data.JSON); // LOG the response from the backend
+    } catch (error) {
+      console.error("Error", error);
+    }
+  };
+
   return (
-    <div className="controls">
+    <div className='controls'>
       <p>
         <label>Name</label>
         <input
-          type="text"
+          type='text'
           value={name}
           onChange={(e) => setName(e.target.value)}
         />
@@ -27,7 +54,7 @@ const RegistrationForm = ({ onClose }) => {
       <p>
         <label>Email</label>
         <input
-          type="email"
+          type='email'
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
@@ -35,7 +62,7 @@ const RegistrationForm = ({ onClose }) => {
       <p>
         <label>Password</label>
         <input
-          type="password"
+          type='password'
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
@@ -43,12 +70,12 @@ const RegistrationForm = ({ onClose }) => {
       <p>
         <label>Confirm Password</label>
         <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
+          type='password'
+          value={confirmPassword}
+          onChange={(e) => setconfirmPassword(e.target.value)}
         />
       </p>
-      <button className="button" onClick={handleRegister}>
+      <button className='button' onClick={handleRegister}>
         Register
       </button>
     </div>
