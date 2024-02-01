e44a3a77a10295a47c642035a910cf3fbb69a53c
Nil <95134227+NirejBatrai@users.noreply.github.com>
2024-02-01 23:25:54 +0700

    added backend code and connect with sql

--- Backend/server.js
+++ Backend/server.js
@@ -1,18 +1,39 @@
 const express = require("express");
 const mysql = require("mysql");
+const dotenv = require("dotenv");
 const cors = require("cors");
 
-const app = express();
-app.use(cors());
+dotenv.config({ path: "./.env" });
 
+const app = express();
+
+app.use(express.json()); // Middleware to parse JSON request bodies
+app.use(cors()); // Enable CORS for all routes
 
 const db = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"",
-   
-})
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE,
+});
 
-app.listen(8081, () => {
-  console.log("listing...");
+db.connect((error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("MYSQL Connected...");
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send("<h1>Home</h1>");
+});
+
+app.post("/register", (req, res) => {
+  console.log(req.body);
+  res.send("Registration form submitted");
+});
+
+app.listen(5001, () => {
+  console.log("Server is running on port 5001...");
 });
