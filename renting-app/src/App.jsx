22460fd1cca742d143ea79ad15ff698e5d101eb9
Nil <95134227+NirejBatrai@users.noreply.github.com>
2024-02-03 17:21:59 +0700

    added new page(contact)

--- renting-app/src/App.jsx
+++ renting-app/src/App.jsx
@@ -5,19 +5,21 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Login from "./pages/Login";
 import { Route, Routes } from "react-router-dom";
-import BikeDetails from "./pages/BIkeDetails";
+import BikeDetails from "./pages/BikeDetails";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
     <>
       <Navbar />
-      <div className="container">
+      <div className='container'>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/pricing" element={<Pricing />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/bike-details" element={<BikeDetails />} />
+          <Route path='/' element={<Home />} />
+          <Route path='/pricing' element={<Pricing />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/bike-details' element={<BikeDetails />} />
+          <Route path='/contact' element={<Contact />} />
         </Routes>
       </div>
     </>
